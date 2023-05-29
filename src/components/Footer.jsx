3b303c0dcecf911631b5105a0f62f8cca0f979db
marcellinrabe <marcellinp20.aps1a@gmail.com>
2023-05-29 14:35:37 +0300

    add hover effect for social links

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -11,13 +11,13 @@ export default function Footer() {
                 </div>
                 <div className="flex justify-center gap-2">
                     <div>
-                        <a href="https://www.facebook.com/rabemarcellin"><RiFacebookCircleFill size={32} /></a>
+                        <a href="https://www.facebook.com/rabemarcellin"><RiFacebookCircleFill size={32} className="hover:text-gray-500" /></a>
                     </div>
                     <div>
-                        <a href="https://linkedin.com/in/marcellinrabe"><RiLinkedinFill size={32} /></a>
+                        <a href="https://linkedin.com/in/marcellinrabe"><RiLinkedinFill size={32} className="hover:text-gray-500" /></a>
                     </div>
                     <div>
-                        <a href="https://www.github.com/marcellinrabe"><RiGithubFill size={32} /></a>
+                        <a href="https://www.github.com/marcellinrabe"><RiGithubFill size={32} className="hover:text-gray-500" /></a>
                     </div>
                 </div> 
 
